e27683876b0c29d590f3c83850982319bbbb3581
Rafaelunisimon <r_ruiz2@unisimon.edu.co>
2026-04-22 20:45:27 -0500

    Create Car.js

---
+++ model/Car.js
@@ -0,0 +1,30 @@
+const model = {
+    currentCar: null,
+    cars: [
+        {
+            clickCount: 0,
+            name: "Coupe Maserati",
+            imgSrc: "img/black-convertible-coupe.jpg",
+        },
+        {
+            clickCount: 0,
+            name: "Camaro SS 1LE",
+            imgSrc: "img/chevrolet-camaro.jpg",
+        },
+        {
+            clickCount: 0,
+            name: "Dodger Charger 1970",
+            imgSrc: "img/dodge-charger.jpg",
+        },
+        {
+            clickCount: 0,
+            name: "Ford Mustang 1966",
+            imgSrc: "img/ford-mustang.jpg",
+        },
+        {
+            clickCount: 0,
+            name: "190 SL Roadster 1962",
+            imgSrc: "img/mercedes-benz.jpg",
+        },
+    ],
+}; 
